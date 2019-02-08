c5e141a975423e12f930b93e350e322639a58204
PK Banks <pkbanks12@gmail.com>
2019-02-08 12:06:36 -0500

    add routes, controllers, seed data, and Api::V1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :stores
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :products
+      resources :stores
+      resources :users
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pk = User.create(
+  email: "pkbanks@gmail.com",
+  password: "pkpkpk"
+)
+
+puts "new user created: #{pk.email}" if pk
+
+mario = User.create(
+  email: "mario@popgage.co",
+  password: "mariomariomario"
+)
+
+puts "new user created: #{mario.email}" if mario
+
+bss = Store.create(
+  name: "Brickell Smoke Shop"
+)
+
+puts "new store created: #{bss.name}" if bss
+
+mango = Product.create(
+  name: "JUUL Mango 5%",
+  description: "yummy mango flavor",
+  price: 24.99,
+  store: bss
+)
+
+puts "new product created: #{mango.name}" if mango
+
+mint = Product.create(
+  name: "JUUL Mint 5%",
+  description: "minty freshness",
+  price: 19.99,
+  store: bss
+)
+
+puts "new product created: #{mint.name}" if mint
+
+puts "BSS products:"
+bss.products.each do |p|
+  puts "> #{p.name} @ $#{p.price}"
+end
+
+puts "--- end ---"
